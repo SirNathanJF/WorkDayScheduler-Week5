974ca6afaf09f97a9789bacdf14c3e52b2d9ea69
Nathan Flessner <nathanflessner1@gmail.com>
2021-07-25 14:49:25 -0600

    added for loop to add text from local storage if local storage is present

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,13 @@ const presentDay = moment();
 $("#currentDay").text(presentDay.format("dddd, MMMM Do"));
 
 $(".container").on("click", ".saveBtn", sendToLocalStorage);
+
+for(let eventText = 9; eventText <= 17; eventText++){
+    let schedule = localStorage.getItem("hour" + eventText);
+    $("#hour${eventText}").text(schedule);
+
+    if($("#hour${eventText}").text() == null){
+        $("#hour${eventText}").text().empty();
+    }
+};
+
