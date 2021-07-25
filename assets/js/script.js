7a9d5b4ec2d5eed1795f12579ba448037f8407b6
Nathan Flessner <nathanflessner1@gmail.com>
2021-07-25 15:29:14 -0600

    fixed jquery syntax error in getting local storage, added function to save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,32 @@ $(".container").on("click", ".saveBtn", sendToLocalStorage);
 
 for(let eventText = 9; eventText <= 17; eventText++){
     let schedule = localStorage.getItem("hour" + eventText);
-    $("#hour${eventText}").text(schedule);
+    $(`#hour${eventText}`).text(schedule);
 
-    if($("#hour${eventText}").text() == null){
-        $("#hour${eventText}").text().empty();
+    if($(`#hour${eventText}`).text() == null){
+        $(`#hour${eventText}`).text().empty();
     }
 };
 
 for (let hourIndex = 9; hourIndex <= 17; hourIndex++){
-    const presentTime = $("#hour${hourIndex}").data("time")
+    const presentTime = $(`#hour${hourIndex}`).data("time")
 
     if(parseInt(presentDay.format("H")) > presentTime){
-        $("#hour${hourIndex}").addClass("past");
+        $(`#hour${hourIndex}`).addClass("past");
 
     } else if(parseInt(presentDay.format("H")) == presentTime){
-        $("#hour${hourIndex}").addClass("present");
+        $(`#hour${hourIndex}`).addClass("present");
 
     } else{
-        $("#hour${hourIndex}").addClass("future");
+        $(`#hour${hourIndex}`).addClass("future");
     }
+};
+
+function sendToLocalStorage(event){
+    event.preventDefault();
+
+    let timeClicked = $(this).parent("section").children("textarea").data("time");
+    let savedEvent = $(this).parent("section").children("textarea").val();
+
+    localStorage.setItem("hour" + timeClicked, savedEvent);
 };
